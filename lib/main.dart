77f6b1f7d6037aa5a6c9c61bc2bd68b5529fd40b
AHSANIQBAL8586 <ahsaniqbal8586@gmail.com>
2021-01-08 20:48:05 +0500

    login page design created with decent colors:

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-
 import 'dart:io';
 // import 'package:quizzler/retry.dart';
 // import 'quiz_brain.dart';
@@ -37,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
     Timer(
         Duration(seconds: 6),
             () => Navigator.pushReplacement(
-                context, MaterialPageRoute(builder: (context) => QuizApp())));
+                context, MaterialPageRoute(builder: (context) => NOTEAPP())));
   } //<- Creates an object that fetches an image.
   // var image = new Image(
   //     image: AssetImage(
@@ -73,21 +72,117 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-class QuizApp extends StatelessWidget {
+class NOTEAPP extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
-        body: SafeArea(
-            child: Text("Wait for another work")
-          // Padding(
-          //   // padding: EdgeInsets.symmetric(horizontal: 10.0),
-          //   child: QuizPage(),
-          // ),
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                colors: [
+                  Colors.pink[400],
+                  Colors.pink[200],
+                  Colors.blue[100],
+                ]
+            )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 80,),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                   Text("Login page", style: TextStyle(color: Colors.white, fontSize: 40),),
+                  SizedBox(height: 10,),
+                   Text("Welcome Back", style: TextStyle(color: Colors.white, fontSize: 18),),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.blue[100],
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20))
+                ),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 60,),
+                         Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [BoxShadow(
+                                  color: Color.fromRGBO(225, 95, 27, .3),
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10)
+                              )]
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey[200]))
+                                ),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      hintText: "Email or Phone number",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey[200]))
+                                ),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      hintText: "Password",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 40,),
+                         Text("Forgot Password?", style: TextStyle(color: Colors.grey),),
+                        SizedBox(height: 40,),
+                         Container(
+                          height: 50,
+                          margin: EdgeInsets.symmetric(horizontal: 50),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                            color: Colors.blue
+                          ),
+                          child: Center(
+                            child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                          ),
+                        ),
+                        SizedBox(height: 50,),
+                         Text("Continue with social media", style: TextStyle(color: Colors.grey),),
+
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
-       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
